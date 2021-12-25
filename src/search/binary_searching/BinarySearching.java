836fecc3816bc97cc55792cb64650df6fcd4431e
Andrey Smirnov <andreysm1996@mail.ru>
2021-12-25 23:37:28 +0300

    refactoring

--- src/search/binary_searching/BinarySearching.java
+++ src/search/binary_searching/BinarySearching.java
@@ -7,10 +7,10 @@ package search.binary_searching;
 
 public class BinarySearching {
 
-    private static int[] list = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+    private static final int[] SORTED_LIST = {11, 22, 33, 44, 55, 66, 77, 88, 99};
 
     public static void main(String[] args) {
-        System.out.println(binarySearching(list,12));
+        System.out.println(binarySearching(SORTED_LIST,12));
     }
 
     private static boolean binarySearching(int[] array, int search) {
